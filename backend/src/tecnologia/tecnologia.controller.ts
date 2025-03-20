5b63dc21229ee73e090d64fde9de977beec67c87
Nobre 81 <jnobr3c+github@gmail.com>
2025-03-20 17:07:22 -0300

    :construction: 31 - API de Projeto

--- backend/src/tecnologia/tecnologia.controller.ts
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { TecnologiaProvider } from './tecnologia.provider';
+import { TecnologiaPrisma } from './tecnologia.prismar';
 import { Tecnologia } from '@core';
 
 @Controller('tecnologias')
 export class TecnologiaController {
-    constructor(private readonly repo: TecnologiaProvider){}
+    constructor(private readonly repo: TecnologiaPrisma){}
 
     @Get()
     async obterTodas(): Promise<Tecnologia[]> {
